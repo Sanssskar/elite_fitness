2973f9f95284e6e4310d5dcdfa289644d086bfc8
Sanssskar <your-email@example.com>
2026-08-19 21:37:53 +0545

    starting project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('Frontend.home');
-});
+Route::get('/', [PageController::class,'home'])->name('home');
